0832606b573b3b36581275697e5ab13a8287e047
James Williams <jameswilliams@catalyst-au.net>
2021-07-14 19:43:33 +1000

    Multi-Local-Docker: Add templates, script to apply templates, README.md

--- README.md
+++ README.md
@@ -26,11 +26,82 @@ git clone git@github.com:moodle/moodle.git siteroot
 cp moodle-config siteroot/config.php
 ```
 
-4. Start containers
+4. (Optional) Edit config.php to enable mailcatcher
+
+```
+vim siteroot/config.php
+```
+
+Uncomment the following line
+```
+// $CFG->smtphosts = "mailcatcher:25";
+```
+And comment out the line:
+```
+$CFG->noemailever = true;
+```
+
+5. Start containers
 
 ```
 docker-compose up
 ```
+
+## Self-Signed Certificates and Multiple Docker Environments
+
+1. Stop/Destroy containers
+
+```
+docker-compose down
+```
+
+2. Decide on hostname, method and ports for HTTP, HTTPS and MAILCATCHER
+
+e.g.
+```
+DOCKER_PORT=81
+DOCKER_PORT_SSL=44381
+DOCKER_PORT_MAILCATCHER=1081
+WWWROOT_HOSTNAME=docker-01-44381.local
+WWWROOT_METHOD=https
+WWWROOT_PORT=44381
+```
+
+3. Overwrite docker files from template using script (and generate self-signed SSL cert):
+
+```
+./apply-template.sh 81 44381 1081 docker-01-44381.local https 44381
+```
+Or, if you've copied/pasted the variable above into a terminal/environment variables:
+```
+./apply-template.sh $DOCKER_PORT $DOCKER_PORT_SSL $DOCKER_PORT_MAILCATCHER $WWWROOT_HOSTNAME $WWWROOT_METHOD $WWWROOT_PORT
+```
+
+
+4. Add WWWROOT_HOSTNAME as an additional entry in your host PC's /etc/hosts file
+
+```
+sudo vim /etc/hosts
+```
+
+Add WWWROOT_HOSTNAME to end of 127.0.0.1 line, like so:
+
+```
+127.0.0.1   localhost docker-01-44381.local
+```
+
+5. Start containers
+
+```
+docker-compose up
+```
+
+6. Access container in browser
+
+https://docker-01-44381.local (will redirect to https://docker-01-44381.local:44381). 
+
+After typing it in once, now you can use your browser's autocomplete awesome bar by typing "docker-01-" ... or "docker-02-" ... and you don't need to remember custom port numbers.
+
 
 ## Utility Commands
 
---
+++ apply-template.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+#
+
+DOCKER_PORT=$1
+DOCKER_PORT_SSL=$2
+DOCKER_PORT_MAILCATCHER=$3
+WWWROOT_HOSTNAME=$4
+WWWROOT_METHOD=$5
+WWWROOT_PORT=$6
+
+# ==================================
+# ===== TEMPLATED DOCKER FILES =====
+# ==================================
+
+# docker-compose.yml
+cp -a docker-compose-template.yml docker-compose.yml
+sed -i "s/%%DOCKER_PORT%%/$DOCKER_PORT/g" docker-compose.yml
+sed -i "s/%%DOCKER_PORT_SSL%%/$DOCKER_PORT_SSL/g" docker-compose.yml
+sed -i "s/%%DOCKER_PORT_MAILCATCHER%%/$DOCKER_PORT_MAILCATCHER/g" docker-compose.yml
+
+
+# docker/moodle/nginx-site
+cp -a docker/moodle/nginx-site-template docker/moodle/nginx-site
+sed -i "s/%%DOCKER_PORT%%/$DOCKER_PORT/g" docker/moodle/nginx-site
+sed -i "s/%%DOCKER_PORT_SSL%%/$DOCKER_PORT_SSL/g" docker/moodle/nginx-site
+sed -i "s/%%WWWROOT_HOSTNAME%%/$WWWROOT_HOSTNAME/g" docker/moodle/nginx-site
+sed -i "s/%%WWWROOT_METHOD%%/$WWWROOT_METHOD/g" docker/moodle/nginx-site
+sed -i "s/%%WWWROOT_PORT%%/$WWWROOT_PORT/g" docker/moodle/nginx-site
+
+# moodle-config to siteroot/config.php
+mkdir -p siteroot
+cp -a moodle-config-template siteroot/config.php
+sed -i "s/%%WWWROOT_HOSTNAME%%/$WWWROOT_HOSTNAME/g" siteroot/config.php
+sed -i "s/%%WWWROOT_METHOD%%/$WWWROOT_METHOD/g" siteroot/config.php
+sed -i "s/%%WWWROOT_PORT%%/$WWWROOT_PORT/g" siteroot/config.php
+sed -i "s/%%DOCKER_PORT_MAILCATCHER%%/$DOCKER_PORT_MAILCATCHER/g" siteroot/config.php
+
+# moodle/docker/Dockerfile
+cp -a docker/moodle/Dockerfile-template docker/moodle/Dockerfile
+sed -i '/^ENTRYPOINT .*/i COPY nginx-selfsigned.key /etc/ssl/private/nginx-selfsigned.key\nCOPY nginx-selfsigned.crt /etc/ssl/certs/nginx-selfsigned.crt\nCOPY dhparam.pem /etc/ssl/certs/dhparam.pem\nCOPY self-signed.conf /etc/nginx/snippets/self-signed.conf\nCOPY ssl-params.conf /etc/nginx/snippets/ssl-params.conf\n\n' docker/moodle/Dockerfile
+
+# ======================================
+# ===== SELF-SIGNED SSL CERTIFICATE ====
+# ======================================
+
+# Generate private key and cert with WWWROOT_HOSTNAME
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ./docker/moodle/nginx-selfsigned.key -out ./docker/moodle/nginx-selfsigned.crt \
+    -subj "/CN=$WWWROOT_HOSTNAME\/emailAddress=admin@$WWWROOT_HOSTNAME/C=AU/ST=/L=/O=Catalyst IT Locla Docker/OU=" 
+
+# Generate strong Diffie-Hellman group
+openssl dhparam -out ./docker/moodle/dhparam.pem 2048
+
+# Generate/overwrite any ssl-params.conf (already packaged, but hey):
+cat << 'EOF' > ./docker/moodle/ssl-params.conf
+# from https://cipherli.st/
+# and https://raymii.org/s/tutorials/Strong_SSL_Security_On_nginx.html
+
+ssl_protocols TLSv1 TLSv1.1 TLSv1.2;
+ssl_prefer_server_ciphers on; 
+ssl_ciphers "EECDH+AESGCM:EDH+AESGCM:AES256+EECDH:AES256+EDH";
+ssl_ecdh_curve secp384r1;
+ssl_session_cache shared:SSL:10m;
+ssl_session_tickets off;
+ssl_stapling on; 
+ssl_stapling_verify on; 
+resolver 8.8.8.8 8.8.4.4 valid=300s;
+resolver_timeout 5s; 
+# Disable preloading HSTS for now.  You can use the commented out header line that includes
+# the "preload" directive if you understand the implications.
+#add_header Strict-Transport-Security "max-age=63072000; includeSubdomains; preload";
+add_header Strict-Transport-Security "max-age=63072000; includeSubdomains";
+add_header X-Frame-Options DENY;
+add_header X-Content-Type-Options nosniff;
+
+ssl_dhparam /etc/ssl/certs/dhparam.pem;
+
+EOF
+
